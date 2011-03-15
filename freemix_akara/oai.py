ab3cf49e49666a3378bded91460ce9a4b8818bed
Uche <uche@ogbuji.net>
2011-03-14 19:42:09 -0600

    Implement OAI ListSets

--- freemix_akara/oai.py
+++ freemix_akara/oai.py
@@ -11,57 +11,27 @@ python contentdm_adapter.py http://digital.library.louisville.edu/cdm4/ "crutche
 
 import sys, time
 
-import amara
-from amara import bindery
-from amara import tree
 from amara.thirdparty import json
-from amara.bindery.html import parse as htmlparse
-from amara.bindery.util import dispatcher, node_handler
-from amara.lib.iri import absolutize
-from amara.lib.util import first_item
 
 from akara.services import simple_service
 from akara import logger
 from akara import module_config
 
-from zen.geo import geolookup
-from zen.contentdm import read_contentdm
+from zen.oai import oaiservice
 
-try:
-    import objgraph #http://mg.pov.lt/objgraph/
-    def checkmem(): objgraph.show_most_common_types(limit=5)
-except ImportError:
-    def checkmem(): pass
+SERVICE_ID = 'http://purl.org/com/zepheira/freemix/services/oai/listsets'
 
-DEFAULT_SITE = 'http://digital.library.louisville.edu/cdm4/'
-SERVICE_ID = 'http://purl.org/com/zepheira/freemix/services/contentdm.json'
+@simple_service('GET', SERVICE_ID, 'oai.listsets.json', 'application/json')
+def oai(endpoint='http://dspace.mit.edu/oai/request', limit=100):
+    """
+    """
+    limit = int(limit)
+    remote = oaiservice(endpoint, logger)
+    sets = remote.list_sets()[:limit]
+    #logger.debug("Start URL: " + repr(url))
+    #logger.debug("Limit: {0}".format(limit))
+    return json.dumps(sets, indent=4)
 
-@simple_service('GET', SERVICE_ID, 'contentdm.json', 'application/json')
-def contentdm(collection='all', query=None, site=DEFAULT_SITE, limit=None):
-    '''
-    Search all collections in Louisville:
-
-    curl "http://localhost:8880/contentdm.json?query=crutches&site=http://digital.library.louisville.edu/cdm4/&limit=100"
-
-    Search just /jthom collection in Louisville:
-
-    curl "http://localhost:8880/contentdm.json?collection=/jthom&query=crutches&site=http://digital.library.louisville.edu/cdm4/&limit=100"
-
-    Search all collections in U Miami:
-
-    curl "http://localhost:8880/contentdm.json?query=crutches&site=http://doyle.lib.muohio.edu/cdm4/&limit=100"
-    '''
-    limit = int(limit) if limit else None
-    results = read_contentdm(site, collection=collection, query=query, limit=limit, logger=logger)
-    header = results.next()
-    url = header['basequeryurl']
-    count = 0
-    logger.debug("Start URL: " + repr(url))
-    logger.debug("Limit: {0}".format(limit))
-    entries = list(results)
-    logger.debug("Result count: {0}".format(len(entries)))
-    #checkmem()
-    return json.dumps({'items': entries, 'data_profile': PROFILE}, indent=4)
 
 PROFILE = {
     #"original_MIME_type": "application/vnd.ms-excel", 
